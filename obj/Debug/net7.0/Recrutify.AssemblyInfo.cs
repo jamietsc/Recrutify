99c07ac1b54a6532218bbbb962388f8698f1037e
jamietsc <jamie.jentsch2007@gmail.com>
2024-09-25 14:55:18 +0200

    Alles nochmal

--- obj/Debug/net7.0/Recrutify.AssemblyInfo.cs
+++ obj/Debug/net7.0/Recrutify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recrutify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021211c492568c1d4b2bbef36bb51855a964e5d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de483886139cc4fe56c4f9e7d5294d5e688c9830")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recrutify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recrutify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
