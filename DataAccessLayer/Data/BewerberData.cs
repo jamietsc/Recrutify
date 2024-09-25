a8a8d739b90731075466d6853025c4e4b0214218
jamietsc <jamie.jentsch2007@gmail.com>
2024-09-25 23:47:19 +0200

    Update BewerberData.cs
    Create a Sql Query to insert the first and last name into the table

--- DataAccessLayer/Data/BewerberData.cs
+++ DataAccessLayer/Data/BewerberData.cs
@@ -1,4 +1,4 @@
-﻿using Recrutify.DataAccessLayer.Repository;
+﻿using Recrutify.DataAccessLayer.Repositories;
 using Recrutify.DataAccessLayer.SqlDataAccess;
 using Recrutify.Models;
 using System.Collections.Generic;
@@ -10,15 +10,26 @@ namespace Recrutify.DataAccessLayer.Data
     public class BewerberData : IBewerber<BewerberModel>
     {
         private readonly ISqlDataAccess _db;
-        public BewerberData(ISqlDataAccess db) {
+        public BewerberData(ISqlDataAccess db)
+        {
             _db = db;
         }
 
-    public async Task<int> InsertVornameNachname(BewerberModel model)
-    {
+        public async Task<int> InsertVornameNachname(BewerberModel model)
+        {
+            // Definierung der Parameter für die SQL-Abfrage
             var parameters = new { model.Vorname, model.Nachname };
-            int insertedID = await _db.SaveDataReturnID("sp", parameters);
+
+            // SQL-Abfrage ohne den Prefix "model."
+            string sqlQuery = "INSERT INTO Bewerber (Vorname, Nachname) VALUES (@Vorname, @Nachname); SELECT last_insert_rowid();";
+
+            // Aufruf der SaveDataReturnID Methode, um die ID des eingefügten Datensatzes zu erhalten
+            int insertedID = await _db.SaveDataReturnID(sqlQuery, parameters);
+
+            // Setze die BID des BewerberModels auf die eingefügte ID
             model.BID = insertedID;
+
             return insertedID;
+        }
     }
 }
