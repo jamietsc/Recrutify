c79a55a15b532e1d7ce546659356d3adb6eacce6
jamietsc <jamie.jentsch2007@gmail.com>
2024-10-04 16:48:14 +0200

    Create AdminData.cs
    Data Class for the Admin Page/Login

---
+++ DataAccessLayer/Data/AdminData.cs
@@ -0,0 +1,23 @@
+﻿using Recrutify.DataAccessLayer.SqlDataAccess;
+using Recrutify.DataAccessLayer.Repositories;
+using Recrutify.Models;
+using System.Numerics;
+
+namespace Recrutify.DataAccessLayer.Data
+{
+    public class AdminData
+    {
+        private readonly ISqlDataAccess _db;
+        public AdminData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public async Task<int> CheckCredentials(AdminModel model)
+        {
+            var parameters = new { model.Benutzername, model.Passwort };
+            string sqlQuery = "SELECT Benutzername = @Benutzername, Passwort = @Passwort FROM Unternehmen;";
+            return await _db.LoadData(sqlQuery, parameters);
+        }
+    }
+}
