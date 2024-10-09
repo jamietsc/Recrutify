9edc7498b2d45634b5c672e5978625e0d13c2a4e
jamietsc <jamie.jentsch2007@gmail.com>
2024-10-09 10:07:26 +0200

    Update IBewerber.cs

--- DataAccessLayer/Repositories/IBewerber.cs
+++ DataAccessLayer/Repositories/IBewerber.cs
@@ -1,9 +1,12 @@
-﻿namespace Recrutify.DataAccessLayer.Repositories
+﻿using Recrutify.Models;
+
+namespace Recrutify.DataAccessLayer.Repositories
 {
     public interface IBewerber<T> where T : class
     {
         Task<int> InsertVornameNachname(T model);
-
         Task InsertPunktzahl(T model);
+
+        Task<List<BewerberModel>> GetEverything(List<int> bewerberID);
     }
 }
