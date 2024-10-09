e5239e9881a1297761660779c079d8fbcbd55cb4
jamietsc <jamie.jentsch2007@gmail.com>
2024-10-09 10:04:31 +0200

    temp data

--- obj/Debug/net7.0/Recrutify.AssemblyInfo.cs
+++ obj/Debug/net7.0/Recrutify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recrutify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f800ef17770b84a504de422093beaf5592488de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+385d0e01e3d8d8565534ea03dacaf265b1ffb7a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recrutify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recrutify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
