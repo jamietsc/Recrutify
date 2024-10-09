f4fc7e209df296ee49a9da560e42c71529bc7985
jamietsc <jamie.jentsch2007@gmail.com>
2024-10-09 10:07:49 +0200

    Update AdminModel.cs
    Added a TID to save it in the admin (admin page) model

--- Models/AdminModel.cs
+++ Models/AdminModel.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string Benutzername { get; set; } = string.Empty;
         public string Passwort { get; set; } = string.Empty;
-
+        public int TID{ get; set; }
     }
 }
